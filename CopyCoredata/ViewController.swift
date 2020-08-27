69c71f4773bdf620883bcabce968f60b2248f7c6
Mparra123 <mauricioparrales@hotmail.com>
2020-08-27 00:20:25 -0600

    Change #2 about the bug of core data

--- CopyCoredata/ViewController.swift
+++ CopyCoredata/ViewController.swift
@@ -11,8 +11,9 @@ import CoreData
 
 class ViewController: UIViewController {
 
-    @IBAction func guardar(_ sender: Any) {
-    }
+        var users = [NSManagedObject]()
+    
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var lastNametext: UITextField!
@@ -22,26 +23,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    extension ViewController: UITableViewDataSource, UITableViewDelegate {
-        
+    
+    @IBAction func guardar(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+             let context =  appDelegate.persistentContainer.viewContext
+             
+             let userEntity = NSEntityDescription.entity(forEntityName: "User", in: context)
+             let user = NSManagedObject(entity: userEntity!, insertInto: context) as! User
+             
+             user.name = nameText.text
+             user.lastName = lastNametext.text
+             user.email = emailText.text
+             
+             do {
+                 try context.save()
+                 users.append(user)
+                 tableView.reloadData()
+             }catch let error {
+                 print("Error \(error.localizedDescription)")
+             }
     }
     
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+           return 1
+       }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return users.count
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let user = users[indexPath.row] as? User
-        cell.textLabel?.text = "\(user?.name ?? "") \(user?.lastName ?? "")"
-        return cell
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+               let user = users[indexPath.row] as? User
+               cell.textLabel?.text = "\(user?.name ?? "") \(user?.lastName ?? "")"
+               return cell
     }
     
-
+ 
 }
-
